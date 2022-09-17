af4a02008936a9a2d09e46287ea5131b9c2d7e31
2chanwoo <navillela.808@gmail.com>
2022-09-18 05:33:11 +0900

    route

--- second/app.js
+++ second/app.js
@@ -5,19 +5,14 @@ const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"> <input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
+app.use(adminRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.send('<h1>Hello from Express!');
-});
 
 app.listen(3000);
