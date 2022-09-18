f106c66c9dfe8bda519f7d4088a51222e8f6c118
2chanwoo <navillela.808@gmail.com>
2022-09-18 21:35:30 +0900

    궁금증 해결.      '/..' => '..'

--- second/routes/admin.js
+++ second/routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // get: /admin/add-product
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '/..', 'views', 'add-product.html'));
+    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
 // post: /admin/add-product
--- second/routes/shop.js
+++ second/routes/shop.js
@@ -4,8 +4,8 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    // 직접 경로를 적어도 되지만, 리눅스는 슬래시, 윈도우는 백슬래시라 다를 수  있음. //!<< '/..' 슬래시 쓰는데?
-    res.sendFile(path.join(__dirname, '/..', 'views', 'shop.html'));
+    // 직접 경로를 적어도 되지만, 리눅스는 슬래시, 윈도우는 백슬래시라 다를 수  있음.
+    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
