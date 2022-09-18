a33e9e2eeed899665cc19dd2fa9c9938fb98325d
2chanwoo <navillela.808@gmail.com>
2022-09-18 21:31:50 +0900

    HTML 페이지 서비스

--- second/routes/shop.js
+++ second/routes/shop.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    // 직접 경로를 적어도 되지만, 리눅스는 슬래시, 윈도우는 백슬래시라 다를 수  있음. //!<< '/..' 슬래시 쓰는데?
+    res.sendFile(path.join(__dirname, '/..', 'views', 'shop.html'));
 });
 
 module.exports = router;
