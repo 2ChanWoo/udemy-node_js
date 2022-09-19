f62a949280d2c37a42558c1394a11df19b59bb6e
2chanwoo <navillela.808@gmail.com>
2022-09-20 00:29:53 +0900

    route

--- second/routes/admin.js
+++ second/routes/admin.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const rootDIr = require('../util/path');
 
 const router = express.Router();
 
 // get: /admin/add-product
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDIr, 'views', 'add-product.html'));
 });
 
 // post: /admin/add-product
