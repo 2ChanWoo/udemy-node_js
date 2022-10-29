382ad2cf39f3f92beba6fb94eabe25bfcc326eec
2chanwoo <navillela.808@gmail.com>
2022-10-29 18:23:33 +0900

    DB sequelize.define

--- second/models/product.js
+++ second/models/product.js
@@ -1,32 +1,27 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-const Cart = require('./cart')
+const sequelize = require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  //! INSERT 에서, VALUES 값을 ? 로 하는 이유는, SQL injection 을 방지하기 위하여 숨기기 위함이다.
-  save() {
-    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-    [this.title, this.price, this.imageUrl, this.description]);
-  }
-
-  //udemy code
-  static deleteById(id) {
- 
-  }
-
-  static fetchAll(cb) {
-  return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id, cb) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-};
+module.exports = Product;
--- second/util/database.js
+++ second/util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'udemy_schema',
-    password: 'cksdnR23'
+const sequelize = new Sequelize('udemy_schema', 'root', 'cksdnR23', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
